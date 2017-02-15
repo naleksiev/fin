8c7d5f61cc1750c4a5d39380f3adb650915caea1
Nikolay Aleksiev <aleksiev.nikolay@gmail.com>
2017-02-14 21:31:58 -0800

    Added FileWrite

--- src/mod/fin_io.c
+++ src/mod/fin_io.c
@@ -15,10 +15,22 @@ static void fin_io_write_line(fin_ctx* ctx, fin_val* args) {
     printf("%s\n", fin_str_cstr(args[0].s));
 }
 
+static void fin_io_file_write(fin_ctx* ctx, fin_val* args) {
+    const char* file = fin_str_cstr(args[0].s);
+    if (!file)
+        return;
+    FILE* fp = fopen(file, "wb");
+    if (!fp)
+        return;
+    fputs(fin_str_cstr(args[1].s), fp);
+    fclose(fp);
+}
+
 void fin_io_register(fin_ctx* ctx) {
     fin_mod_func_desc descs[] = {
         { "void Write(string)", &fin_io_write },
         { "void WriteLine(string)", &fin_io_write_line },
+        { "void FileWrite(string,string)", &fin_io_file_write },
     };
 
     fin_mod_create(ctx, "io", descs, FIN_COUNT_OF(descs));
